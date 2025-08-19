f1229e3b7effceb209212c2a2154af7e7e3aa4a1
Ulyana <6291709@gmail.com>
2025-08-19 10:10:00 +0200

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -6,11 +6,11 @@ def calculate(numbers):
     else:
         arr=np.array(numbers).reshape(3,3)
         calculations = {
-            "mean" : [list(arr.mean(axis=0)), list(arr.mean(axis=1)), arr.mean()],
-            "var" : [list(arr.var(axis=0)), list(arr.var(axis=1)), arr.var()],
-            "std" : [list(arr.std(axis=0)), list(arr.std(axis=1)), arr.std()],
-            "max" : [list(arr.max(axis=0)), list(arr.max(axis=1)), arr.max()],
-            "min" : [list(arr.min(axis=0)), list(arr.min(axis=1)), arr.min()],
-            "sum" : [list(arr.sum(axis=0)), list(arr.sum(axis=1)), arr.sum()],
+            "mean": [arr.mean(axis=0).tolist(), arr.mean(axis=1).tolist(), arr.mean().tolist()],
+            "var": [arr.var(axis=0).tolist(), arr.var(axis=1).tolist(), arr.var().tolist()],
+            "std": [arr.std(axis=0).tolist(), arr.std(axis=1).tolist(), arr.std().tolist()],
+            "max": [arr.max(axis=0).tolist(), arr.max(axis=1).tolist(), arr.max().tolist()],
+            "min": [arr.min(axis=0).tolist(), arr.min(axis=1).tolist(), arr.min().tolist()],
+            "sum": [arr.sum(axis=0).tolist(), arr.sum(axis=1).tolist(), arr.sum().tolist()],
         }
     return calculations
